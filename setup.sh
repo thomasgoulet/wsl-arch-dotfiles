63a57a795de90b7b4009c13f0c97ecabbd30e2d6
Thomas Goulet <thomas@jfgoulet.com>
2022-07-16 18:54:24 -0400

    Add paru, github-cli, neofetch

--- setup.sh
+++ setup.sh
@@ -5,6 +5,24 @@
 mkdir -p ~/wsl-arch-dotfiles/.config
 ln -f --symbolic ~/wsl-arch-dotfiles/.config ~/.config
 
+# Paru
+
+## Set this to 1 to install paru
+PARU_INSTALL=0
+
+if [ $PARU_INSTALL -eq 1 ]
+then
+
+	sudo pacman -S --needed base-devel
+	git clone https://aur.archlinux.org/paru.git
+	cd paru
+	makepkg -si
+	cd ..
+	rm -R -f paru
+
+fi
+
+
 # Packages
 
-sudo pacman -S man-db
+sudo pacman -S github-cli man-db neofetch
