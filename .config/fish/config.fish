a50e7a84a464058126dcef3c7e6e83159df0c35e
Thomas Goulet <thomas@jfgoulet.com>
2022-07-17 00:33:05 -0400

    Add some stuff to fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,3 +3,16 @@ if status is-interactive
 end
 
 set fish_greeting
+
+function bind_bang
+    switch (commandline -t)[-1]
+        case "!"
+            commandline -t $history[1]; commandline -f repaint
+        case "*"
+            commandline -i !
+    end
+end
+
+function fish_user_key_bindings
+    bind ! bind_bang
+end
