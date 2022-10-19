4d21ba0d3af3fa3313bda3d6992bbe68f801beb5
thomasgoulet <thomas@jfgoulet.com>
2022-10-19 01:30:25 -0700

    Add command duration on the right side

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -75,3 +75,11 @@ function fish_prompt
     printf ' ↪ '
     set_color normal
 end
+
+function fish_right_prompt
+    if test $CMD_DURATION
+        # Show duration of the last command in seconds
+        set duration (echo "$CMD_DURATION 1000" | awk '{printf "%.3fs", $1 / $2}')
+        echo $duration
+    end
+end
