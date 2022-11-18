69c7fef3b70fa05d9270da4a04f7ddcf98072d4e
Thomas Goulet <Thomas.Goulet@dfo-mpo.gc.ca>
2022-11-18 16:33:05 -0500

    Add helm completion

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -74,6 +74,7 @@ end
 ## Kubernetes
 
 kubectl completion fish | source
+helm completion fish | source
 
 function kcon
     kubectl config use-context (kubectl config get-contexts | fzf --height 10% --reverse --inline-info --bind 'tab:down' --bind 'shift-tab:up' --delimiter=' ' --nth=2.. --header-lines=1 | cut -c 2- | awk '{print $1}')
