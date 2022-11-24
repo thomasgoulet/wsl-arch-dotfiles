d411636de7967012291e895f120a5969e5bdd3f7
Thomas Goulet <thomas.goulet@dfo-mpo.gc.ca>
2022-11-24 14:33:12 -0500

    Fix setup.sh paru install

--- setup.sh
+++ setup.sh
@@ -5,14 +5,9 @@
 mkdir -p ~/wsl-arch-dotfiles/.config
 ln -f --symbolic ~/wsl-arch-dotfiles/.config ~/.config
 
-if [ $INSTALL_PARU -eq 1 ]
-then
-
-	sudo pacman -S --needed base-devel
-	git clone https://aur.archlinux.org/paru.git
-	cd paru
-	makepkg -si
-	cd ..
-	rm -R -f paru
-
-fi
+sudo pacman -S --needed base-devel
+git clone https://aur.archlinux.org/paru.git
+cd paru
+makepkg -si
+cd ..
+rm -R -f paru
