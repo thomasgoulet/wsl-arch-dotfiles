4fdcbbffa2e384593841b0ebc893cb3925fe77ab
Thomas Goulet <thomas@jfgoulet.com>
2023-01-22 02:22:42 -0500

    Bind escape to 'fg'

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -18,6 +18,7 @@ end
 function fish_user_key_bindings
     bind ! bind_bang
     bind \cr 'commandline -i "$(history | fzf --height 40% --reverse --inline-info --tiebreak length --bind \'tab:down\' --bind \'shift-tab:up\')"'
+    bind \e 'fg;commandline -f repaint'
 end
 
 # Macros / Shortcuts
