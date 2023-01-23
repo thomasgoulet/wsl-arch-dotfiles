0c0dc3e5c1cd48fdcf50f387397fdd930c3a96fb
Thomas Goulet <thomas@jfgoulet.com>
2023-01-23 18:12:59 -0500

    Better sort in profiling fucntion

--- .config/fish/functions/fish_profile_startup.fish
+++ .config/fish/functions/fish_profile_startup.fish
@@ -1,6 +1,6 @@
 # Profile fish shell startup time
 function fish_profile_startup
     fish --profile-startup /tmp/fish.profile -i -c exit
-    sort -k1nr /tmp/fish.profile
+    sort -k2n /tmp/fish.profile
     rm /tmp/fish.profile
 end
