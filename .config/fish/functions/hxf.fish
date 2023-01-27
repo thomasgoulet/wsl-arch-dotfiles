4bdb4a9c1f1278b52d36c25b9caa10ac911fed2a
thomasgoulet <thomas@jfgoulet.com>
2023-01-26 21:26:21 -0500

    Improve hxf to be full screen and show more context in preview

--- .config/fish/functions/hxf.fish
+++ .config/fish/functions/hxf.fish
@@ -1,8 +1,8 @@
 # Open helix inside a sub-directory chosen via fzf
 function hxf
     set -f hx_file (rg -n -H . |\
-        fzf --height 70% --reverse --inline-info --tiebreak length --bind "tab:down" --bind "shift-tab:up" -d : \
-        --preview "bat -n --color=always -r (math max 0, {2}-10): -H {2} {1}" |\
+        fzf --inline-info --tiebreak index --preview-window right,60% --bind "tab:up" --bind "shift-tab:down" -d : \
+        --preview "bat -n --color=always -r (math max 0, {2}-15): -H {2} {1}" |\
         cut -d : -f 1,2)
     if test "$hx_file" = ""
         return
