1efe8e3c3b7ac5d464dd4391799ad7444440c6c5
Thomas Goulet <thomas@jfgoulet.com>
2023-03-09 21:11:48 -0500

    Try the ZelliJ split planes

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -11,7 +11,6 @@ set fish_greeting
 function fish_user_key_bindings
     bind ! bind_bang
     bind \cr 'commandline -i "$(history | fzf --height 40% --reverse --inline-info --tiebreak length --bind \'tab:down\' --bind \'shift-tab:up\')"'
-    bind \e 'zellij action toggle-floating-panes'
 end
 
 zoxide init fish --cmd j | source
